aa3dde1fad21f8ee8fd23d66f6d93d31b23f17d4
iKest <ikestster@gmail.com>
2020-11-18 15:09:52 +0300

    Add files via upload

---
+++ fic/src/settings.4e310.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default","capture"],collisionMatrix:[[true,true],[true,true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scenes/Upload Scene.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"fbb78",resources:"23eea",main:"03e6a"}};
